e70812e79ac188425ec586d4683a824dddd4f6a9
Veronica Orozco <93060985+VLOrozco@users.noreply.github.com>
2022-07-31 14:23:19 -0500

    Update main.js
    created script for getValue() & submitFunction() to get selected rating and output the rating into the thank you container response

---
+++ main.js
@@ -0,0 +1,37 @@
+// RATING START CONTAINER
+const ratingStartContainer = document.getElementById('rating-state-start');
+console.log(ratingStartContainer);
+
+// THANK YOU START CONTAINER
+const tyStartContainer = document.getElementById('ty-box');
+console.log(tyStartContainer);
+
+
+
+// GET VALUE() & OUTPUT TO STAR RATING ON TY CONTAINER
+function getValue() {
+  // FINAL SELECTED RATING
+  const starRating = document.getElementById('rating');
+
+  // OUTPUT SELECTED RATING
+  starRating.innerText = this.value;
+}
+
+// GET BUTTONS AND DECLARE AS RATES
+var rates = document.querySelectorAll('.rtg');
+
+// FOR EACH RATE | ON CLICK | CALL getValue FUNCTION
+rates.forEach(rate => {
+  rate.addEventListener('click', getValue, true);
+})
+
+
+
+// SUBMIT BUTTON
+const submit = document.getElementById('submit');
+
+// SUBMIT FUNCTION
+function submitFunction() {
+  ratingStartContainer.style.display = "none";
+  tyStartContainer.style.display = "block";
+}
